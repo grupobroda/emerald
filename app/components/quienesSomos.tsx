23acc318fabc640be5aeb2c433899606609d12b4
Juan Ignacio García <juanigarciadev@gmail.com>
2025-03-27 17:08:24 -0300

    Se aplican cambios a los breakpoints

--- app/components/quienesSomos.tsx
+++ app/components/quienesSomos.tsx
@@ -53,21 +53,21 @@ export default function QuienesSomos() {
     return (
         <section className="flex flex-col gap-20 p-20 pt-30 px-80 xl:px-20" id="quienesSomos">
             <div className="grid grid-cols-2 gap-10 xl:flex xl:flex-col">
-                <div className="flex flex-col gap-5 border border-neutral-300 p-5 rounded-lg">
-                    <div className="inline-flex items-center gap-3">
+                <div className="flex flex-col gap-5 border border-neutral-300 p-5 rounded-lg md:p-5">
+                    <div className="inline-flex items-center gap-3 md:flex-col">
                         <div className="flex items-center justify-center bg-[#218F7F]/45 h-20 w-20 rounded-lg">
                             <PersonIcon/>
                         </div>
-                        <h3 className="text-5xl font-bold">¿Quiénes somos?</h3>
+                        <h3 className="text-5xl font-bold md:text-center">¿Quiénes somos?</h3>
                     </div>
                     <p className="text-neutral-600 text-xl">Unidad financiera de Grupo Broda, empresa de triple impacto global que con originalidad: invierte, desarrolla y gestiona modelos de negocios sostenibles.</p>
                 </div>
                 <div className="flex flex-col gap-5 border border-neutral-300 p-5 rounded-lg">
-                    <div className="inline-flex items-center gap-3">
+                    <div className="inline-flex items-center gap-3 md:flex-col">
                         <div className="flex items-center justify-center bg-[#218F7F]/45 h-20 w-20 rounded-lg">
                             <BullseyeIcon/>
                         </div>
-                        <h3 className="text-5xl font-bold">Objetivo</h3>
+                        <h3 className="text-5xl font-bold md:text-center">Objetivo</h3>
                     </div>
                     <p className="text-neutral-600 text-xl">Con un fuerte equipo de profesionales, nos enfocamos en acompañar a nuestros clientes en el desarrollo tanto de sus finanzas tanto personales como corporativas, brindando herramientas locales y externas que optimicen sus inversiones.</p>
                 </div>
