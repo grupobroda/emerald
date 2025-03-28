8f44c6eed278e58919a1a316d7838019aa35e704
Juan Ignacio García <juanigarciadev@gmail.com>
2025-03-28 15:33:38 -0300

    Se pasan a escala de grises las imagenes de la sección de Información y se optimiza el tamaño

--- app/components/informacion.tsx
+++ app/components/informacion.tsx
@@ -5,19 +5,19 @@ const INFO_MOCK = [
     {
         titulo: "Personas",
         texto: "Ayudamos a potenciar tus ahorros, mediante la inversión en mercado de capitales, teniendo en cuenta tus objetivos, perfil de riesgo y horizonte temporal.",
-        imagen: "https://res.cloudinary.com/dgbsqdjqy/image/upload/f_auto,q_auto/xjalfo9rrqasdug4plzm",
+        imagen: "https://res.cloudinary.com/dgbsqdjqy/image/upload/f_auto,q_auto/r5i0wgqcjrwjx28nj7r7",
         url: "/personas"
     },
     {
         titulo: "Empresas",
         texto: "Asesoramos a empresas en sus necesidades de manejo de flujo de caja y de financiamiento.",
-        imagen: "https://res.cloudinary.com/dgbsqdjqy/image/upload/f_auto,q_auto/vgd6ldj2dbz88cnl5p0g",
+        imagen: "https://res.cloudinary.com/dgbsqdjqy/image/upload/f_auto,q_auto/g8wtgd73yygonsiyf415",
         url: "/empresas"
     },
     {
         titulo: "Multi Family Office",
         texto: "Apoyamos a las familias empresarias en la gestión de su patrimonio, aconsejando sobre buenas prácticas para la preservación del mismo.",
-        imagen: "https://res.cloudinary.com/dgbsqdjqy/image/upload/f_auto,q_auto/ew0gog4cybcbsppit6vd",
+        imagen: "https://res.cloudinary.com/dgbsqdjqy/image/upload/f_auto,q_auto/vr3aom1znrx98opfmbbq",
         url: "/familyoffice"
     }]
 
@@ -37,7 +37,7 @@ export default function Informacion() {
                         <div className="absolute inset-0 bg-[#034941]/60 w-full h-full rounded-lg"></div>
                         <h2 className="text-white font-bold text-3xl z-10">{info.titulo}</h2>
                         <span className="text-white z-10">{info.texto}</span>
-                        <button className="text-white bg-neutral-300/15 border px-6 py-3 rounded-full w-fit group-hover:bg-white group-hover:text-black group-hover:border-transparent duration-200 z-10">Más información</button>
+                        <button className="text-white bg-neutral-300/15 border px-6 py-3 rounded-full w-fit group-hover:bg-white group-hover:text-black group-hover:border-transparent duration-200 z-10 cursor-pointer">Más información</button>
                     </Link>
                 )
             })}
