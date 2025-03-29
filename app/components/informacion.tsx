b3745a1566ad72f53e6a8ce379888564e698ef31
Juan Ignacio García <juanigarciadev@gmail.com>
2025-03-29 09:45:44 -0300

    Se modifica el comportamiento de los botones de Más información en dispositivos móviles

--- app/components/informacion.tsx
+++ app/components/informacion.tsx
@@ -12,8 +12,8 @@ export default function Informacion() {
                         <div className="absolute inset-0 bg-[#034941]/60 w-full h-full rounded-lg"></div>
                         <h2 className="text-white font-bold text-3xl z-10">{info.titulo}</h2>
                         <span className="text-white z-10">{info.texto}</span>
-                        <button aria-label={"Más información sobre " + info.titulo} className="relative flex items-center justify-center text-white bg-neutral-300/15 border p-3 rounded-full z-10 cursor-pointer transition-[width] duration-300 overflow-hidden w-12 group-hover:w-48 group-hover:bg-white group-hover:text-black group-hover:border-transparent">
-                            <span className="absolute whitespace-nowrap opacity-0 group-hover:relative group-hover:opacity-100 transition-opacity duration-300">
+                        <button aria-label={"Más información sobre " + info.titulo} className="relative flex items-center justify-center text-white bg-neutral-300/15 border p-3 rounded-full z-10 cursor-pointer transition-[width] duration-300 overflow-hidden w-12 group-hover:w-48 group-hover:bg-white group-hover:text-black group-hover:border-transparent lg:w-48">
+                            <span className="absolute whitespace-nowrap opacity-0 group-hover:relative group-hover:opacity-100 transition-opacity lg:relative lg:opacity-100 duration-300">
                                 Más información
                             </span>
                             <ChevronRight  />
