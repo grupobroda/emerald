db56f5e0a4acd2d2dad9b9903bd50c0dcdbd5cc7
Juan Ignacio García <juanigarciadev@gmail.com>
2025-03-29 08:54:13 -0300

    Se modifican los botones de las cards en la sección Información

--- app/components/informacion.tsx
+++ app/components/informacion.tsx
@@ -1,17 +1,23 @@
 import Link from "next/link";
 import React from "react";
 import {INFO_MOCK} from "../lib/mocks";
+import ChevronRight from "../assets/icons/chevronright"
 
 export default function Informacion() {
     return(
         <section className="grid grid-cols-3 p-20 pt-30 gap-5 lg:flex lg:flex-col md:px-10">
             {INFO_MOCK.map((info)=> {
                 return(
-                    <Link href={info.url} key={info.titulo} className="flex flex-col items-center text-center py-30 gap-10 bg-white p-10 rounded-lg bg-center bg-cover bg-no-repeat relative group cursor-pointer translateTransition" style={{backgroundImage: `url(${info.imagen})`}}>
+                    <Link href={info.url} key={info.titulo} className="relative flex flex-col items-center text-center py-30 gap-10 bg-white p-10 rounded-lg bg-center bg-cover bg-no-repeat group cursor-pointer translateTransition" style={{backgroundImage: `url(${info.imagen})`}}>
                         <div className="absolute inset-0 bg-[#034941]/60 w-full h-full rounded-lg"></div>
                         <h2 className="text-white font-bold text-3xl z-10">{info.titulo}</h2>
                         <span className="text-white z-10">{info.texto}</span>
-                        <button className="text-white bg-neutral-300/15 border px-6 py-3 rounded-full w-fit group-hover:bg-white group-hover:text-black group-hover:border-transparent duration-200 z-10 cursor-pointer">Más información</button>
+                        <button className="relative flex items-center justify-center text-white bg-neutral-300/15 border p-3 rounded-full z-10 cursor-pointer transition-[width] duration-300 overflow-hidden w-12 group-hover:w-48 group-hover:bg-white group-hover:text-black group-hover:border-transparent">
+                            <span className="absolute whitespace-nowrap opacity-0 group-hover:relative group-hover:opacity-100 transition-opacity duration-300">
+                                Más información
+                            </span>
+                            <ChevronRight  />
+                        </button>
                     </Link>
                 )
             })}
